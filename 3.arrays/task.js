e29f8745ce556aaa5c4a01d4a33555bb61494b65
SergeyMeschaninov <Liga.severa@yandex.ru>
2026-02-06 13:51:14 +0300

    Howe work commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,19 @@
 function compareArrays(arr1, arr2) {
-  
+
+  if (arr1.length !== arr2.length) return false;
+
+  return arr1.every((el, index) => el === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+  let filteredAges = users
+    .filter(user => user.gender === gender)
+    .map(user => user.age);
+
+  if(filteredAges.length === 0){
+      return 0;
+  }
+
+  let totalSum = filteredAges.reduce((sum, currentValue) => sum + currentValue, 0);
+  return totalSum / filteredAges.length;
 }
